79bdaee84cad9e4defa3fa6353f3b9b2f5bb71a5
Sayantan <sayantandatta97@gmail.com>
2018-05-05 22:34:34 +0530

    Create factorial.js

---
+++ factorial.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+// your code goes here
+var num;
+var solution = 1;
+
+for(var num=12; num > 1; num--){     
+       solution *= num;
+    }
+console.log(solution);
