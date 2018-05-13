9525eac2a125a3d57017b9c4cea770a58c55f7f2
Sayantan <sayantandatta97@gmail.com>
2018-05-13 21:07:59 +0530

    Create reoomoveDOM_element.js

---
+++ reoomoveDOM_element.js
@@ -0,0 +1,10 @@
+/*
+For this quiz, remove the <ul> from the first article item!
+
+You must use jQuery's remove() method.
+*/
+
+// Start with this variable!
+var articleItems;
+
+articleItems = $(".article-item").find('ul').remove(); //serach in article-item class then find all ul then remove it from the web .
