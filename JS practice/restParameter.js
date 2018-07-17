56d77d124955d2573bf6b2c039d3c256956316b9
Sayantan <sayantandatta97@gmail.com>
2018-07-17 22:21:33 +0530

    rest parameter is used for arrays
    It is handy  . It starts from ...(triple dot) . ex - ...num

---
+++ JS practice/restParameter.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: Avarage Using the Rest Parameter (1-5)
+ */
+
+// your code goes here
+
+function average(...theArgs) {
+    let temp = 0;
+    for(const theArg of theArgs){
+    temp+=theArg; 
+    }
+    return temp/(theArgs.length);
+}
+
+console.log(average(2, 6));
+console.log(average(2, 3, 3, 5, 7, 10));
+console.log(average(7, 1432, 12, 13, 100));
+console.log(average());
